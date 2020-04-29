a9995281fdda672cc1328e99ebe7d630472f8854
Unknown <nutella.daniella@gmail.com>
2020-04-29 13:39:28 +0200

    hellopy script with return vaule
    (doesnt work)

--- Backend/hello.py
+++ Backend/hello.py
@@ -1,10 +1,21 @@
 #!python
 from distutils.core import setup, Extension
+import numpy as np
+import ctypes
 
-HelloModule = Extension('HelloModule',
+arrVector = np.array([1, 2, 3, 4])
+def vector():
+    return Extension
+
+HelloModule = vector()('HelloModule',
                     sources = ['hello.cpp'])
 
 setup (name = 'HelloModule',
        version = '1.0',
        description = 'This is a demo package',
        ext_modules = [HelloModule])
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+
+print(arr.shape)
