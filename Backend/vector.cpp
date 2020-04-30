93bd227299384797ceb84ec9d01b56d9384abae8
Unknown <nutella.daniella@gmail.com>
2020-04-30 21:40:23 +0200

    Add Vector class

--- Backend/vector.cpp
+++ Backend/vector.cpp
@@ -2,6 +2,61 @@
 #include<Python.h>
 #include<arrayobject.h>
 
+class Vector {
+
+	float* data;
+	int size;
+	Vector(float* data, int size) {
+
+		this->data = (float*)malloc(sizeof(float)* size);
+		this->size = size;
+		for (int i = 0; i < size; i++)
+		{
+			this->data[i] = data[i];
+		}
+	};
+
+	Vector(PyArrayObject* arrayVector) {
+	
+		float* data = (float*)PyArray_DATA(arrayVector);
+		npy_intp* ndShape1 = PyArray_SHAPE(arrayVector);
+		this->data = (float*)malloc(sizeof(float) * ndShape1[0]);
+		this->size = ndShape1[0];
+		for (int i = 0;  i < size;  i++)
+		{
+			this->data[i] = data[i];
+		}
+	}
+
+	Vector operator+(Vector& vector) {
+		if (this->size != vector.size)
+		{
+			std::cout << "Vector legth mismatch" << std::endl;
+			return NULL;
+		}
+		float* sum = (float*)malloc(sizeof(float) * vector.size);
+		for (int i = 0; i < vector.size; i++)
+		{
+			sum[i] = this->data[i] + vector.data[i];
+		}
+		Vector vendetta = Vector(sum, vector.size);
+		free(sum);
+		return vendetta;
+	};
+
+	PyArrayObject* toNumpy() {
+		npy_intp vectorSize[] = { this->size };
+		return (PyArrayObject*)PyArray_SimpleNewFromData(1, vectorSize, NPY_FLOAT32, (void*)this->data);
+	}
+
+	~Vector() {
+		free(data);
+	};
+};
+
+
+
+
 static PyObject*
 vector(PyObject* self, PyObject* arg) {
 
@@ -11,7 +66,7 @@ vector(PyObject* self, PyObject* arg) {
 	if (vector1 == NULL || vector2 == NULL)
 	{
 		std::cout << "Argument parse failed" << std::endl;
-		return 0;
+		return NULL;
 	}
 	PyArrayObject* arrayVector1;
 	PyArrayObject* arrayVector2;
@@ -20,13 +75,25 @@ vector(PyObject* self, PyObject* arg) {
 	arrayVector2 = (PyArrayObject*)PyArray_ContiguousFromAny(vector2, PyArray_FLOAT32, 0, 2, NPY_ARRAY_DEFAULT, NULL);
 	if (arrayVector1 == NULL || arrayVector2 == NULL) {
 		std::cout << "Arrayobject cast failed" << std::endl;
-		return 0;
+		return NULL;
+	}
+	int dims1 = PyArray_NDIM(arrayVector1);
+	int dims2 = PyArray_NDIM(arrayVector2);
+	if (dims1 != 1 || dims2 != 1) {
+		std::cout << "Argument is not a vector" << std::endl;
+		return NULL;
 	}
 	float sum;
 	float* data = (float*)PyArray_DATA(arrayVector1);
-	npy_intp* ndShape = PyArray_SHAPE(arrayVector1);
+	npy_intp* ndShape1 = PyArray_SHAPE(arrayVector1);
 	float* data2 = (float*)PyArray_DATA(arrayVector2);
-	int size = ndShape[0];
+	npy_intp* ndShape2 = PyArray_SHAPE(arrayVector2);
+	if (ndShape1[0] != ndShape2[0]) {
+		std::cout << "Vector lenght mismatch" << std::endl;
+		return NULL;
+	}
+
+	int size = ndShape1[0];
 	float* vectorSum = (float*)malloc(sizeof(float) * size);
 	for (int i = 0; i < size; i++)
 	{
